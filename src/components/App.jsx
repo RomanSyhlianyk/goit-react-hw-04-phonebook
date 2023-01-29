b9527f11197b589cef638acdda2212db2d033e2d
RomanSyhlianyk <romansyglianyk@gmail.com>
2023-01-29 09:53:57 +0200

    clear ref

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
 import { nanoid } from 'nanoid';
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect } from 'react';
 import { ContactForm } from './ContactForm/ContactForm';
 import { ContactList } from './ContactList/ContactList';
 import css from './App.module.css';
